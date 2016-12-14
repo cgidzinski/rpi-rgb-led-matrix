19e1f1b3921baa03580a949f9f1bb82b9f70b1a7
Colin Gidzinski <evg31337@gmail.com>
2016-12-14 18:23:28 -0500

    ddd

--- python/samples/stockD.py
+++ python/samples/stockD.py
@@ -52,7 +52,7 @@ class RunText(SampleBase):
 
 
             if (textQueue[x][0] == (offscreenCanvas.width - lenTop-8)):
-                if x <= len(textData):
+                if x <= len(textData)-1:
                     x+=1
                     textQueue.append([offscreenCanvas.width,x])
                 else:

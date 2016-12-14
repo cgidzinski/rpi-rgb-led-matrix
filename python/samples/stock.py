3fde74153fc0197eacd9e9c8527cf5ad64c068ca
Colin Gidzinski <evg31337@gmail.com>
2016-12-14 02:23:39 -0500

    realign

--- python/samples/stock.py
+++ python/samples/stock.py
@@ -20,12 +20,12 @@ class RunText(SampleBase):
 
         while True:
             offscreenCanvas.Clear()
-            len = graphics.DrawText(offscreenCanvas, font, pos, 14, textColor, myText)
+            len = graphics.DrawText(offscreenCanvas, font, pos, 11, textColor, myText)
             pos += 1
             if (pos + len > offscreenCanvas.width):
                 pos = 0
-            time.sleep(5)
-            time.sleep(0.025)
+            time.sleep(0.5)
+            # time.sleep(0.025)
             offscreenCanvas = self.matrix.SwapOnVSync(offscreenCanvas)
 
 

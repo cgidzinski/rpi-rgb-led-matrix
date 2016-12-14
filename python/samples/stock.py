3d743e492fabfce34f0b97aa1fc2cd5b94fb57c4
Colin Gidzinski <evg31337@gmail.com>
2016-12-14 04:48:57 -0500

    again

--- python/samples/stock.py
+++ python/samples/stock.py
@@ -36,10 +36,12 @@ class RunText(SampleBase):
 
             totalOffsetTop = 0
             for x in xrange(0,len(textTop)):
-                graphics.DrawText(offscreenCanvas, fontBig, posA+(totalOffsetTop), 11, textTop[x][3], textTop[x][0])
-                lenTop = graphics.DrawText(offscreenCanvas, fontBig, posA+(totalOffsetTop), 22, textTop[x][3], textTop[x][1] +" ("+ textTop[x][2]+")")
-
-                totalOffsetTop += (lenTop+8)
+                lenTopA = graphics.DrawText(offscreenCanvas, fontBig, posA+(totalOffsetTop), 11, textTop[x][3], textTop[x][0])
+                lenTopB = graphics.DrawText(offscreenCanvas, fontBig, posA+(totalOffsetTop), 22, textTop[x][3], textTop[x][1] +" ("+ textTop[x][2]+")")
+                if lenTopA > lenTopB:
+                    totalOffsetTop += (lenTopA+8)
+                else:
+                    totalOffsetTop += (lenTopB+8)
 
          
             

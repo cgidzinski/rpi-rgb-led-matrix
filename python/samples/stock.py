404b96a5b00b4293f43bfd3ba0b9f6816f677a90
Colin Gidzinski <evg31337@gmail.com>
2016-12-14 04:32:23 -0500

    again

--- python/samples/stock.py
+++ python/samples/stock.py
@@ -33,8 +33,8 @@ class RunText(SampleBase):
             offscreenCanvas.Clear()
             textDate = strftime("%B %d, %Y", localtime())
             textTime = strftime("%H:%M:%S", localtime())
-            graphics.DrawText(offscreenCanvas, fontSmall, 1, 30, white, textDate)
-            graphics.DrawText(offscreenCanvas, fontSmall, 208, 30, white, textTime)
+            graphics.DrawText(offscreenCanvas, fontSmall, 1, 31, white, textDate)
+            graphics.DrawText(offscreenCanvas, fontSmall, 208, 31, white, textTime)
 
             totalOffsetTop = 0
             for x in xrange(0,len(textTop)):

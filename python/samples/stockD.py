b174f7bb830319a6424f43319f35c699df615b64
Colin Gidzinski <evg31337@gmail.com>
2016-12-14 18:05:07 -0500

    d

--- python/samples/stockD.py
+++ python/samples/stockD.py
@@ -32,13 +32,15 @@ class RunText(SampleBase):
             textTime = strftime("%H:%M:%S", localtime())
             graphics.DrawText(offscreenCanvas, fontSmall, 1, 31, white, textDate)
             graphics.DrawText(offscreenCanvas, fontSmall, 208, 31, white, textTime)
+
             x=0
             totalLength = 0
+
             lenTop = graphics.DrawText(offscreenCanvas, fontBig, posA, 11, textData[x][4], textData[x][0]+" "+textData[x][1]+" "+textData[x][2] +" ("+ textData[x][3]+")")
             totalLength += (lenTop+8)
 
-            if (posA == (offscreenCanvas.width - lenTop)):
-                posA = 0
+            if (posA == (offscreenCanvas.width - lenTop+8)):
+                lenTop = graphics.DrawText(offscreenCanvas, fontBig, totalLength + posA, 11, textData[x][4], textData[x][0]+" "+textData[x][1]+" "+textData[x][2] +" ("+ textData[x][3]+")")
             posA-=1
 
 

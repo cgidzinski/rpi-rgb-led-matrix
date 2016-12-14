d15fba12ee01b64e63a42e0fb7127f431024fd9a
Colin Gidzinski <evg31337@gmail.com>
2016-12-14 18:19:05 -0500

    d

--- python/samples/stockD.py
+++ python/samples/stockD.py
@@ -51,7 +51,7 @@ class RunText(SampleBase):
 
 
 
-            if (posA < 180):
+            if (textQueue[x][0] == (offscreenCanvas.width - lenTop+8)):
                 x+=1
                 textQueue.append([offscreenCanvas.width,x])
 

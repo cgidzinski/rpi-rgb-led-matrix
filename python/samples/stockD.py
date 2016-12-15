92c2693e06799d5a19662bbea0dfc5dbf7e31115
Colin Gidzinski <evg31337@gmail.com>
2016-12-14 20:18:42 -0500

    show all

--- python/samples/stockD.py
+++ python/samples/stockD.py
@@ -26,6 +26,7 @@ def get_value(identifier):
     get_value_url = 'http://finance.google.com/finance/info?client=ig&q=' + identifier 
     value = subprocess.Popen(['curl', '-s', get_value_url], stdout=subprocess.PIPE).communicate()[0]
     j = json.loads(value[5:len(value)-2])
+    print(j)
     return float(j['l'])
 
 def getPrices():

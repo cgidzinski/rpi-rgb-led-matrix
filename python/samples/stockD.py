e58bdb2ea5129ccdb6ed7cf199d915733919b39e
Colin Gidzinski <evg31337@gmail.com>
2016-12-14 20:26:20 -0500

    print data

--- python/samples/stockD.py
+++ python/samples/stockD.py
@@ -26,8 +26,7 @@ def get_value(identifier):
     get_value_url = 'http://finance.google.com/finance/info?client=ig&q=' + identifier 
     value = subprocess.Popen(['curl', '-s', get_value_url], stdout=subprocess.PIPE).communicate()[0]
     j = json.loads(value[5:len(value)-2])
-    print(str(j['24']))
-    return str(j['l'])
+    return [str(j['t']),str(j['l']),str(j['c']),str(j['cp'])]
 
 def getPrices():
     while True:

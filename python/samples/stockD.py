fbb272afef567162d107b3ab699252000d4518f8
Colin Gidzinski <evg31337@gmail.com>
2016-12-14 21:25:50 -0500

    clear fix and date

--- python/samples/stockD.py
+++ python/samples/stockD.py
@@ -10,7 +10,7 @@ import subprocess
 import json
 import os
 #
-speed = 0.025
+speed = 0.03
 fontBig = graphics.Font()
 fontBig.LoadFont("../../fonts/8x13B.bdf")
 fontSmall = graphics.Font()
@@ -42,63 +42,64 @@ def get_value():
             dataItem.append(red)   
         textDataArray.append(dataItem)    
     return textDataArray
-
+#
 def getPrices():
     global textData
     global initData
     while True:
+        os.system("clear")
         print "Getting Update Data"
         textData = get_value()
-        print "Got Data\r\n"
+        print "Got Data at " +strftime("%I:%M:%S %p", localtime()) + "\r\n"
         initData = True
-        time.sleep(10)
-
+        time.sleep(5)
+#
 class RunText(SampleBase):
     def __init__(self, *args, **kwargs):
         super(RunText, self).__init__(*args, **kwargs)
-
+#
     def Run(self):
         self.matrix.brightness = 40
         offscreenCanvas = self.matrix.CreateFrameCanvas()
-
+#
         t = Thread(target=getPrices)
         t.daemon = True
         t.start()
-
+#
         while initData == False:
             time.sleep(.1)
-
+#
         IndexTop=0
         IndexBottom=len(textData)-1
         posTop = offscreenCanvas.width
         posBottom = 0-8*(len(textData[IndexBottom][0])+len(textData[IndexBottom][1])+len(textData[IndexBottom][2])+len(textData[IndexBottom][3])+5)
-        
+#
         while True:
             offscreenCanvas.Clear()
             graphics.DrawText(offscreenCanvas, fontSmall, 1, 31, white, strftime("%B %d, %Y", localtime()))
             graphics.DrawText(offscreenCanvas, fontSmall, 190, 31, white, strftime("%I:%M:%S %p", localtime()))
-
+#
             if len(textQueueTop) == 0:
                 textQueueTop.append([posTop,IndexTop])
-
+#
             if len(textQueueBottom) == 0:
                 textQueueBottom.append([posBottom,IndexBottom])
-
+#
             for i in xrange(0,len(textQueueTop)):
                 lenTop = graphics.DrawText(offscreenCanvas, fontBig, textQueueTop[i][0], 11, textData[textQueueTop[i][1]][4], textData[textQueueTop[i][1]][0] +" "+ textData[textQueueTop[i][1]][1] +" "+ textData[textQueueTop[i][1]][2] +" ("+ textData[textQueueTop[i][1]][3]+")")
                 textQueueTop[i][0] -=1
-
+#
             for i in xrange(0,len(textQueueBottom)):
                 lenBottom = graphics.DrawText(offscreenCanvas, fontBig, textQueueBottom[i][0], 23, textData[textQueueBottom[i][1]][4], textData[textQueueBottom[i][1]][0] +" "+ textData[textQueueBottom[i][1]][1] +" "+ textData[textQueueBottom[i][1]][2] +" ("+ textData[textQueueBottom[i][1]][3]+")")
                 textQueueBottom[i][0] +=1
-
+#
             if (textQueueTop[len(textQueueTop)-1][0] == (offscreenCanvas.width - lenTop-8)):
                 if IndexTop != len(textData)-1:
                     IndexTop+=1
                 else:
                     IndexTop = 0
                 textQueueTop.append([offscreenCanvas.width,IndexTop])
-
+#
             if (textQueueBottom[len(textQueueBottom)-1][0] == 8):
                 if IndexBottom != 0:
                     IndexBottom-=1
@@ -106,7 +107,7 @@ class RunText(SampleBase):
                     IndexBottom = len(textData)-1
                 lenTest = 8*(len(textData[IndexBottom][0])+len(textData[IndexBottom][1])+len(textData[IndexBottom][2])+len(textData[IndexBottom][3])+5)
                 textQueueBottom.append([0-lenTest,IndexBottom])
-           
+#
             time.sleep(speed)
             offscreenCanvas = self.matrix.SwapOnVSync(offscreenCanvas)
 # Main function

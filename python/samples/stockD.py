0a0a71f259592064e2723ae03b8f4f1f7fcaf640
Colin Gidzinski <evg31337@gmail.com>
2016-12-14 21:00:35 -0500

    change bounds

--- python/samples/stockD.py
+++ python/samples/stockD.py
@@ -27,8 +27,8 @@ def get_value():
     identifier = ','.join(symbols)
     get_value_url = 'http://finance.google.com/finance/info?client=ig&q=' + identifier 
     value = subprocess.Popen(['curl', '-s', get_value_url], stdout=subprocess.PIPE).communicate()[0]
-    print(value[5:len(value)-2])
-    j = json.loads(value[5:len(value)-2])
+    print(value[4:len(value)-1])
+    j = json.loads(value[4:len(value)-1])
 
     # returnString = [str(j['t']),str(j['l']),str(j['c']),str(j['cp'])]
     # if float(j['c']) == 0.00:

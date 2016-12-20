f25acf370fdfeebc3a91f6cd09d72fc882c35b85
Colin Gidzinski <evg31337@gmail.com>
2016-12-20 14:59:17 -0500

    neg

--- python/samples/stockD.py
+++ python/samples/stockD.py
@@ -125,7 +125,7 @@ class RunText(SampleBase):
                 lenTest = 8*(len(textData[IndexBottom][0])+len(textData[IndexBottom][1])+len(textData[IndexBottom][2])+len(textData[IndexBottom][3])+5)
                 textQueueBottom.append([0-lenTest,IndexBottom])
 
-            if textQueueTop[0][0] < 400:
+            if textQueueTop[0][0] < -400:
                 textQueueTop.pop(0)
 
             if textQueueBottom[0][0] > 400:

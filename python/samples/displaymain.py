6dfed2a604b7ea835e051ba921b24646921e6803
Colin Gidzinski <evg31337@gmail.com>
2017-11-22 23:24:32 -0500

    test display

---
+++ python/samples/displaymain.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# Display a runtext with double-buffering.
+#sudo ./stockD.py -c 8 -b 50
+from samplebase import SampleBase
+from rgbmatrix import graphics
+from rgbmatrix import RGBMatrix
+import time
+from time import localtime, strftime
+from threading import Thread
+import subprocess
+import json
+import os
+#
+fontBig = graphics.Font()
+fontBig.LoadFont("../../fonts/8x13B.bdf")
+fontSmall = graphics.Font()
+fontSmall.LoadFont("../../fonts/6x10.bdf")
+fontSuper = graphics.Font()
+fontSuper.LoadFont("../../fonts/10x20.bdf")
+green = graphics.Color(0, 255, 0)
+red = graphics.Color( 255, 0, 0)
+blue = graphics.Color(0, 0, 255)
+white = graphics.Color(255, 255, 255)
+
+
+
+class main(SampleBase):
+	def __init__(self, *args, **kwargs):
+		super(RunText, self).__init__(*args, **kwargs)
+#
+	def Run(self):
+		offscreenCanvas = self.matrix.CreateFrameCanvas()
+		offscreenCanvas.Clear()
+		graphics.DrawText(offscreenCanvas, fontSuper, 1, 20, white, "hi")
+		offscreenCanvas = self.matrix.SwapOnVSync(offscreenCanvas)
+		time.sleep(30)
+
+# Main function
+if __name__ == "__main__":
+	parser = main()
+	if (not parser.process()):
+		parser.print_help()

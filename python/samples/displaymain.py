507cd752cc3f81e87a616124e6a9aeab5d770b8a
Colin Gidzinski <colin.gidzinski@shopify.com>
2017-11-23 11:49:50 -0500

    additions

--- python/samples/displaymain.py
+++ python/samples/displaymain.py
@@ -3,27 +3,27 @@
 from samplebase import SampleBase
 from rgbmatrix import graphics
 from rgbmatrix import RGBMatrix
-import json
-import os
-import time
-import commands
-import random
+import json, os, time, commands, random
 #
 import bugsnagCall
 #
-fontBig = graphics.Font()
-fontBig.LoadFont("../../fonts/8x13B.bdf")
 fontSmall = graphics.Font()
 fontSmall.LoadFont("../../fonts/6x10.bdf")
+fontBig = graphics.Font()
+fontBig.LoadFont("../../fonts/8x13B.bdf")
 fontSuper = graphics.Font()
 fontSuper.LoadFont("../../fonts/10x20.bdf")
+#
 green = graphics.Color(0, 255, 0)
 red = graphics.Color( 255, 0, 0)
 blue = graphics.Color(0, 0, 255)
 white = graphics.Color(255, 255, 255)
 orange = graphics.Color(255, 165, 0)
+#
 width = 256
 height = 64
+bugLow = 5
+bugHigh = 10
 #
 slogans = ["Loading Bear Cave","Loading Skynet","Loading Broken Code"]
 #
@@ -33,13 +33,26 @@ class main(SampleBase):
 
     def Run(self):
         def bugsnag(proj, offscreenCanvas):
-            offscreenCanvas.Clear()
             newErrors = bugsnagCall.findErrors(proj,"New")
             ipErrors = bugsnagCall.findErrors(proj,"in_progress")
+            offscreenCanvas.Clear()
+
             graphics.DrawText(offscreenCanvas, fontBig, 1, 11, white, "NEW Bugs: ")
-            graphics.DrawText(offscreenCanvas, fontBig, 72, 11, green, str(len(newErrors)))
+            if len(newErrors) < bugLow:
+                graphics.DrawText(offscreenCanvas, fontBig, 72, 11, green, str(len(newErrors)))
+            else if len(newErrors) > bugHigh:
+                graphics.DrawText(offscreenCanvas, fontBig, 72, 11, orange, str(len(newErrors)))
+            else:
+                graphics.DrawText(offscreenCanvas, fontBig, 72, 11, red, str(len(newErrors)))
+
             graphics.DrawText(offscreenCanvas, fontBig, 1, 29, white, "IP Bugs: ")
-            graphics.DrawText(offscreenCanvas, fontBig, 64, 29, orange, str(len(ipErrors)))
+            if len(ipErrors) < bugLow:
+                graphics.DrawText(offscreenCanvas, fontBig, 64, 29, green, str(len(ipErrors)))
+            else if len(ipErrors) > bugHigh:
+                graphics.DrawText(offscreenCanvas, fontBig, 64, 29, orange, str(len(ipErrors)))
+            else:
+                graphics.DrawText(offscreenCanvas, fontBig, 64, 29, red, str(len(ipErrors)))
+
             offscreenCanvas = self.matrix.SwapOnVSync(offscreenCanvas)
             time.sleep(60)
 
@@ -48,7 +61,6 @@ class main(SampleBase):
         offscreenCanvas = self.matrix.CreateFrameCanvas()
         offscreenCanvas.Clear()
         slogansText = slogans[random.randint(0,len(slogans)-1)]
-        pos = width
         for pos in xrange(width,0,-2):
             offscreenCanvas.Clear()
             graphics.DrawText(offscreenCanvas, fontBig, pos, 11, green, slogansText)

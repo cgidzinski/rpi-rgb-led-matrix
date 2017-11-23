77eec420810821e7016e731a2ac7e1b13f8de87d
Colin Gidzinski <colin.gidzinski@shopify.com>
2017-11-23 16:20:20 -0500

    additions

--- python/samples/displaymain.py
+++ python/samples/displaymain.py
@@ -71,8 +71,9 @@ class main(SampleBase):
             pixels =  list(image.getdata())
             
             index = 0
-            for x in xrange(0,28):
-                offscreenCanvas.SetPixel(x,20,255,0,0)
+            for x in xrange(0,27):
+                for y in xrange(0,27):
+                    offscreenCanvas.SetPixel(x,y,255,0,0)
                 
 
 

15ce663b3dfc8dd805d36bb5849dfdeb3593f39d
Colin Gidzinski <colin.gidzinski@shopify.com>
2017-11-24 10:27:22 -0500

    additions

--- python/samples/displaymain.py
+++ python/samples/displaymain.py
@@ -142,8 +142,8 @@ class main(SampleBase):
         pixels =  list(image.getdata())
         print len(pixels)
         index = 0
-        for y in xrange(0,28):
-            for x in xrange(0,28):
+        for y in xrange(0,32):
+            for x in xrange(0,32):
                 offscreenCanvas.SetPixel(x,y,colors[pixels[index]][0],colors[pixels[index]][1],colors[pixels[index]][2])
                 index += 1
         offscreenCanvas = self.matrix.SwapOnVSync(offscreenCanvas)

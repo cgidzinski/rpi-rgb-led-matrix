a2cd3a8b162a81f74c94d887e01890769524a6ae
Colin Gidzinski <colin.gidzinski@shopify.com>
2017-11-24 11:27:58 -0500

    additions

--- python/samples/displaymain.py
+++ python/samples/displaymain.py
@@ -101,7 +101,7 @@ class main(SampleBase):
             palette= image.im.getpalette()
             colors= [map(ord, bytes) for bytes in chunk(palette, 3)]
             image.seek(0);
-            for z in xrange(1,frames):
+            for z in xrange(0,frames):
                 print z
                 print "of"
                 print frames

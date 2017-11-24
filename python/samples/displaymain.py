89b458704e22a03b2792a77bb095102139bbdeb5
Colin Gidzinski <colin.gidzinski@shopify.com>
2017-11-24 11:32:02 -0500

    additions

--- python/samples/displaymain.py
+++ python/samples/displaymain.py
@@ -151,11 +151,11 @@ class main(SampleBase):
 
         offscreenCanvas = self.matrix.CreateFrameCanvas()
         offscreenCanvas.Clear()
+        offscreenCanvas = self.matrix.SwapOnVSync(offscreenCanvas)
         slogansText = slogans[random.randint(0,len(slogans)-1)]
         graphics.DrawText(offscreenCanvas, fontBig, 36, 12, green, slogansText)
         graphics.DrawText(offscreenCanvas, fontBig, 36, 30, blue, commands.getoutput('hostname -I'))
         drawSquare(offscreenCanvas,white)
-        offscreenCanvas = self.matrix.SwapOnVSync(offscreenCanvas)
         while True:
             showGif(offscreenCanvas, "./bear.gif",0.1)
         #

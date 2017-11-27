31b92f8f65a4fa12b5867333ae0e7d9640133092
Colin Gidzinski <colin.gidzinski@shopify.com>
2017-11-27 11:38:58 -0500

    additions

--- python/samples/displaymain.py
+++ python/samples/displaymain.py
@@ -149,12 +149,12 @@ class main(SampleBase):
                 drawSquare(offscreenCanvas,purple)
                 graphics.DrawLine(offscreenCanvas, 1, height-2, cycle, height-2, orange)
                 graphics.DrawLine(offscreenCanvas, 1, height-1, cycle, height-1, orange)
-                graphics.DrawText(offscreenCanvas, fontBig, 32, 11, green,str(pr['number']))
+                graphics.DrawText(offscreenCanvas, fontBig, 32, 12, green,str(pr['number']))
                 
                 #graphics.DrawText(offscreenCanvas, fontBig, 32+(8*len(str(pr['number']))+1), 11, orange ,pr['labels'] )
                 #graphics.DrawText(offscreenCanvas, fontTiny, 32, 18, white ,pr['title'] )
                 
-                graphics.DrawText(offscreenCanvas, fontBig, 32, 26, orange ,pr['title'] )
+                graphics.DrawText(offscreenCanvas, fontBig, 32, 25, orange ,pr['title'] )
                 offscreenCanvas = self.matrix.SwapOnVSync(offscreenCanvas)
                 offscreenCanvas.Clear()
                 time.sleep(200) 

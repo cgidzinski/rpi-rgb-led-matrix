ed3c9321a8f85cb227244af39cb72727f212cc4f
Colin Gidzinski <colin.gidzinski@shopify.com>
2017-11-27 15:01:06 -0500

    additions

--- python/samples/displaymain.py
+++ python/samples/displaymain.py
@@ -200,6 +200,12 @@ class main(SampleBase):
         graphics.DrawText(offscreenCanvas, fontBig, 34, 26, blue, commands.getoutput('hostname -I'))
         drawSquare(offscreenCanvas,white)
         offscreenCanvas = self.matrix.SwapOnVSync(offscreenCanvas)
+
+        graphics.DrawText(offscreenCanvas, fontBig, 36, 12, green, slogansText)
+        graphics.DrawText(offscreenCanvas, fontBig, 34, 26, blue, commands.getoutput('hostname -I'))
+        drawSquare(offscreenCanvas,white)
+        offscreenCanvas = self.matrix.SwapOnVSync(offscreenCanvas)
+        offscreenCanvas = self.matrix.SwapOnVSync(offscreenCanvas)
         #
         githubCall.setup()
         bugsnagCall.setup()
